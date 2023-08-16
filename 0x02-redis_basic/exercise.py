cc802a1a16fbb9b3d300ae6e833ef0320840e0ea
ken-001-mk <mwaskenne@gmail.com>
2023-08-16 23:22:44 +0300

    the redis👍

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+"""module for using the Redis NoSQL data storage.
+"""
+
+import redis
+import uuid
+from functools import wraps
+from typing import Any, Callable, Union
+
+def count_calls(method: Callable) -> Callable:
+  @wraps(method)
+  def wrapper(self, *args, **kwargs) -> Any:
+    if isinstance(self._redis, redis.Redis):
+      self._redis.incr(method.__qualname__)
+    return method(self, *args, **kwargs)
+  return wrapper
+
+def call_history(method: Callable) -> Callable:
+  @wraps(method)
+  def invoker(self, *args, **kwargs) -> Any:
+    in_key = '{}:inputs'.format(method.__qualname__)
+    out_key = '{}:outputs'.format(method.__qualname__)
+    if isinstance(self._redis, redis.Redis):
+      self._redis.rpush(in_key, str(args))
+    output = method(self, *args, **kwargs)
+    if isinstance(self._redis, redis.Redis):
+      self._redis.rpush(out_key, output)
+    return output
+  return invoker
+
+def replay(fn: Callable) -> None:
+  if fn is None or not hasattr(fn, '__self__'):
+    return
+  redis_store = getattr(fn.__self__, '_redis', None)
+  if not isinstance(redis_store, redis.Redis):
+    return
+  fxn_name = fn.__qualname__
+  in_key = '{}:inputs'.format(fxn_name)
+  out_key = '{}:outputs'.format(fxn_name)
+  fxn_call_count = 0
+  if redis_store.exists(fxn_name) != 0:
+    fxn_call_count = int(redis_store.get(fxn_name))
+  print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+  fxn_inputs = redis_store.lrange(in_key, 0, -1)
+  fxn_outputs = redis_store.lrange(out_key, 0, -1)
+  for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+    print('{}(*{}) -> {}'.format(
+      fxn_name,
+      fxn_input.decode("utf-8"),
+      fxn_output,
+    ))
+
+class Cache:
+  def __init__(self) -> None:
+    self._redis = redis.Redis()
+    self._redis.flushdb(True)
+
+  @call_history
+  @count_calls
+  def store(self, data:  Union[str, bytes, int, float]) -> str:
+    data_key = str(uuid.uuid4())
+    self._redis.set(data_key, data)
+    return data_key
+
+  def get(
+    self,
+    key: str,
+    fn: Callable = None,
+  ) -> Union[str, bytes, int, float]:
+    data = self._redis.get(key)
+    return fn(data) if fn is not None else data
+
+  def get_str(self, key: str) -> str:
+    return self.get(key, lambda x: x.decode('utf-8'))
+
+  def get_int(self, key: str) -> int:
+    return self.get(key, lambda x: int(x))
